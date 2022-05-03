bb147d3f848e7d99056f60840c39d11a895d5aa6
GraceUmutesi <tesigrace98@gmail.com>
2022-05-03 08:18:58 +0200

    assignment completed

---
+++ app.rb
@@ -0,0 +1,16 @@
+#puts is equivalent to console.log in javascript, print in python, println in java or printif in c 
+puts "Hello world!"
+print "Pass this test, please."
+
+#puts [1, 2, 3]
+#will display:
+#1
+#2
+#3
+# we can use p
+p [1, 2, 3]
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+# app.rb
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
